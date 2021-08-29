1c9bfe95deef4cc7b25ae42034189c4649851f17
KuntsevaKateryna <Milka602602>
2021-08-29 07:03:23 +0300

    design corrections

--- src/main/java/com/example/demo/controller/SecurityConfig.java
+++ src/main/java/com/example/demo/controller/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -68,4 +69,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setUserDetailsService(userDetailsServiceImpl);
         return provider;
     }
+
+
 }
